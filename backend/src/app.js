1b2ca7866e1c873b03925c7e5cf8b3bff03e77e7
nwnp-desktop <jjw_0418@naver.com>
2022-06-25 02:34:06 +0900

    delete files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,18 @@
-// const bodyParser = require("body-parser");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const db = require("./db");
-
-// const { sequelize } = require("./models/index");
-// dotenv.config();
-
-// sequelize
-//   .sync({ force: false })
-//   .then(() => {
-//     console.log("db connection success");
-//   })
-//   .catch((err) => {
-//     console.error("db connection error", err);
-//   });
-
-// const apiRoutes = require("./routes/api");
+const db = require('./db');
 
 app.use(express.json());
-// app.use(cors("http://localhost:3000"));
-// app.use("/api", apiRoutes);
-
-app.get("/api/values", (req, res) => {
-  db.pool.query("SELECT * FROM lists;", (err, results, fields) => {
+app.get('/api/values', (req, res) => {
+  db.pool.query('SELECT * FROM lists;', (err, results, fields) => {
     if (err) return res.status(500).send(err);
     else return res.json(results);
   });
 });
 
-app.post("/api/value", (req, res, next) => {
+app.post('/api/value', (req, res, next) => {
   db.pool.query(
     `INSERT INTO lists (value) VALUES("${req.body.value}")`,
     (err, results, fields) => {
@@ -43,5 +23,5 @@ app.post("/api/value", (req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log("The server is on");
+  console.log('The server is on');
 });
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "mysql",
-  database: "myapp",
-  port: 3306,
+  host: 'mysql',
+  user: 'root',
+  password: 'mysql',
+  database: 'myapp',
+  port: 3305,
 });
 
 exports.pool = pool;
